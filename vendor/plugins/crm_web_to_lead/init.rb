61008ddaf1405ba36ca64c6308b946fb89ba7bb1
Karlo <kntsoriano@gmail.com>
2014-05-15 10:13:09 +0800

    [#71370042] Install crm_web_to_lead rails plugin

---
+++ vendor/plugins/crm_web_to_lead/init.rb
@@ -0,0 +1,10 @@
+RAILS_DEFAULT_LOGGER.info ">> Adding web-to-lead Fat Free CRM plugin..."
+
+FatFreeCRM::Plugin.register(:web_to_lead) do
+         name "Web-to-lead Capture Fat Free CRM Plugin"
+       author "Michael Dvorkin"
+      version "1.0"
+  description "Create Fat Free CRM leads from the data submitted via remote web form."
+end
+
+require "crm_web_to_lead.rb"
